59bf4d46bd5e4645d4bdcf1890851e432c97199d
lcchua <laichwang@gmail.com>
2024-09-22 11:37:44 +0800

    Set up the feature toggles to route traffic

--- main.tf
+++ main.tf
@@ -84,23 +84,23 @@ resource "aws_lb_listener" "app" {
 
   default_action {
     type             = "forward"
-  #  target_group_arn = aws_lb_target_group.blue.arn
-    target_group_arn = aws_lb_target_group.green.arn
-    #  forward {
-    #   target_group {
-    #     arn =     aws_lb_target_group.blue.arn
-    #     weight = lookup(local.traffic_dist_map[var.traffic_distribution], "blue", 100)
-    #   }
+    target_group_arn = aws_lb_target_group.blue.arn
+  #  target_group_arn = aws_lb_target_group.green.arn
+     forward {
+      target_group {
+        arn =     aws_lb_target_group.blue.arn
+        weight = lookup(local.traffic_dist_map[var.traffic_distribution], "blue", 100)
+      }
 
-    #   target_group {
-    #     arn =    aws_lb_target_group.green.arn
-    #     weight = lookup(local.traffic_dist_map[var.traffic_distribution], "green", 0)
-    #   }
+      target_group {
+        arn =    aws_lb_target_group.green.arn
+        weight = lookup(local.traffic_dist_map[var.traffic_distribution], "green", 0)
+      }
 
-    #   stickiness {
-    #     enabled  = false
-    #     duration = 1
-    #   }
-    # }
+      stickiness {
+        enabled  = false
+        duration = 1
+      }
+    }
   }
 }
