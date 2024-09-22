8c8de583d54a3b6bb15fb1eca08348cfc3a1ab33
lcchua <laichwang@gmail.com>
2024-09-22 11:27:30 +0800

    Set to gren env only first to test it out independently

--- main.tf
+++ main.tf
@@ -84,8 +84,9 @@ resource "aws_lb_listener" "app" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.blue.arn
-    # forward {
+  #  target_group_arn = aws_lb_target_group.blue.arn
+    target_group_arn = aws_lb_target_group.green.arn
+    #  forward {
     #   target_group {
     #     arn =     aws_lb_target_group.blue.arn
     #     weight = lookup(local.traffic_dist_map[var.traffic_distribution], "blue", 100)
