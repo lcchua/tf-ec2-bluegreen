8e43f9b2bb9ed1bc87d3698373f480d5f87c1923
lcchua <laichwang@gmail.com>
2024-09-22 11:40:39 +0800

    Correct feature toggles to route traffic

--- main.tf
+++ main.tf
@@ -84,7 +84,7 @@ resource "aws_lb_listener" "app" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.blue.arn
+  #  target_group_arn = aws_lb_target_group.blue.arn
   #  target_group_arn = aws_lb_target_group.green.arn
      forward {
       target_group {
