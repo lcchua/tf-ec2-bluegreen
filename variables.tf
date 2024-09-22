00c6b3687dff8eaad5c943deeca8df7b448b8937
lcchua <laichwang@gmail.com>
2024-09-22 11:37:21 +0800

    Set up the feature toggle s to route traffice

--- variables.tf
+++ variables.tf
@@ -22,32 +22,32 @@ variable "enable_green_env" {
   default     = false
 }
 
-# variable "traffic_distribution" {
-#   description = "Levels of traffic distribution"
-#   type        = string
-# }
+variable "traffic_distribution" {
+  description = "Levels of traffic distribution"
+  type        = string
+}
 
-# locals {
-#   traffic_dist_map = {
-#     blue = {
-#       blue  = 100
-#       green = 0
-#     }
-#     blue-90 = {
-#       blue  = 90
-#       green = 10
-#     }
-#     split = {
-#       blue  = 50
-#       green = 50
-#     }
-#     green-90 = {
-#       blue  = 10
-#       green = 90
-#     }
-#     green = {
-#       blue  = 0
-#       green = 100
-#     }
-#   }
-# }
+locals {
+  traffic_dist_map = {
+    blue = {
+      blue  = 100
+      green = 0
+    }
+    blue-90 = {
+      blue  = 90
+      green = 10
+    }
+    split = {
+      blue  = 50
+      green = 50
+    }
+    green-90 = {
+      blue  = 10
+      green = 90
+    }
+    green = {
+      blue  = 0
+      green = 100
+    }
+  }
+}
